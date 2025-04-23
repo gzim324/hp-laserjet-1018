54da1b42a730174e98b988d37ca2f5d112d544ae
Michal Zimka <zimkaxx@gmail.com>
2025-04-23 13:19:03 +0200

    update instruction for user

--- printer.sh
+++ printer.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-# Instalacja pakietów CUPS i sterowników drukarki
 apt update
 apt install -y cups
 usermod -a -G lpadmin root
 cupsctl --remote-any
-apt install -y hplip hplip-data printer-driver-foo2zjs printer-driver-foo2zjs-common printer-driver-pxljr
+apt install -y hplip hplip-data printer-driver-foo2zjs 
+printer-driver-foo2zjs-common printer-driver-pxljr
 
-# Restart usługi CUPS
 systemctl restart cups
 
-# Pobranie aktualnego adresu IP (zakładając, że interfejs to eth0 lub użycie domyślnego)
 IP=$(hostname -I | awk '{print $1}')
 
-# Wyświetlenie instrukcji dla użytkownika
 echo "
 Optional:
-	Set static ip
+	Set static ip:
 	```
 	$ nano /etc/network/interfaces
-	allow-hotplug ens18
-	iface ens18 inet static
-	  address 192.168.0.111
-	  netmask 255.255.255.0
-	  gateway 192.168.0.1
+		allow-hotplug ens18
+		iface ens18 inet static
+		  address 192.168.0.111
+		  netmask 255.255.255.0
+		  gateway 192.168.0.1
+	$ systemctl restart networking  
 	```
 Go to http://$IP:631
 Administration
